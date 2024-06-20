a233e5c7ee0dfb742c94bb246274b96f4bc1df75
Ghulam Muhammad Ahmad <157963398+Ghulam-Muhammad-Ahmad@users.noreply.github.com>
2024-06-20 18:18:39 +0500

    first commit

--- src/components/Addtasks.jsx
+++ src/components/Addtasks.jsx
@@ -3,10 +3,10 @@ export default function Addtasks(props) {
     return (
         <>
             <div className="tasksforms flex flex-col gap-2 justify-center items-center w-full">
-                <div className="w-2/5">
+                <div className="md:w-2/5">
                     {props.taskstate ? <h3 className='text-lg font-bold text-black dark:text-white'>Edit Tasks</h3> : <h3 className=' dark:text-white text-lg font-bold text-black'>Add Tasks</h3>}
                 </div>
-                <label htmlFor="task_title" className='dark:text-white bg-transparent border border-2 border-gray-200 rounded-md p-2 w-2/5'>
+                <label htmlFor="task_title" className='dark:text-white bg-transparent border border-2 border-gray-200 rounded-md p-2 w-full md:w-2/5'>
                     <input type="text"
                         placeholder='Enter Task Title'
                         className='bg-transparent focus:outline-none dark:text-white text-gray-600 w-full'
@@ -15,7 +15,7 @@ export default function Addtasks(props) {
                         value={props.title}
                         id='task_title' />
                 </label>
-                <select name="task_cat" id="task_cat" value={props.taskCat} placeholder="Select Category" className="bg-transparent dark:text-white border border-2 border-gray-200 rounded-md p-2 w-2/5" onChange={(e) => props.settaskCat(e.target.value)}>
+                <select name="task_cat" id="task_cat" value={props.taskCat} placeholder="Select Category" className="bg-transparent dark:text-white border border-2 border-gray-200 rounded-md p-2 w-full md:w-2/5" onChange={(e) => props.settaskCat(e.target.value)}>
                     <option value="" className='dark:text-black'>Select Category</option>
                     {props.cats.map((cat) => {
                         return (
@@ -24,16 +24,16 @@ export default function Addtasks(props) {
                     })
                     }
                 </select>
-                <select name="task_status" id="task_status" value={props.taskStatus} placeholder="Select Status" className="dark:text-white bg-transparent border border-2 border-gray-200 rounded-md p-2 w-2/5" onChange={(e) => props.settaskStatus(e.target.value)}>
+                <select name="task_status" id="task_status" value={props.taskStatus} placeholder="Select Status" className="dark:text-white bg-transparent border border-2 border-gray-200 rounded-md p-2 w-full md:w-2/5" onChange={(e) => props.settaskStatus(e.target.value)}>
                     <option value=""  className='dark:text-black'>Select Status</option>
                     <option value="pending" className='dark:text-black'>Pending</option>
                     <option value="completed" className='dark:text-black'>Completed</option>
                     <option value="blocked" className='dark:text-black'>Blocked</option>
                 </select>
-                <textarea name="task_desc" id='task_desc' placeholder='Enter Task Description' className='bg-transparent h-44 dark:text-white border border-2 border-gray-200 rounded-md p-2 w-2/5' value={props.taskdesc} onChange={(e) => props.settaskdesc(e.target.value)}>
+                <textarea name="task_desc" id='task_desc' placeholder='Enter Task Description' className='bg-transparent h-44 dark:text-white border border-2 border-gray-200 rounded-md p-2 w-full md:w-2/5' value={props.taskdesc} onChange={(e) => props.settaskdesc(e.target.value)}>
                 </textarea>
 
-                <div className="button w-2/5 flex flex-col justify-start items-start">
+                <div className="button md:w-2/5 w-full flex flex-col justify-start items-start">
                     <span className='text-red-600 text-sm font-bold'>
                         {props.taskerror}
                     </span>
@@ -66,7 +66,7 @@ export default function Addtasks(props) {
                             }
                         }}
                         type='button'
-                        className='bg-[#3763d2] dark:bg-white dark:text-black text-white border border-2 border-gray-200 rounded-md p-2 w-2/5'
+                        className='bg-[#3763d2] dark:bg-white dark:text-black text-white border border-2 border-gray-200 rounded-md p-2 w-full md:w-2/5'
                     >
                         {props.taskstate ? "Edit Task" : "Add Task"}
                     </button>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ export default function Main() {
   const [cats, setcat] = useState([])
   return (
   <>
-  <div className="handler flex justify-start align-top mt-3">
+  <div className="handler flex justify-start align-top mt-3 md:flex-row flex-col">
     <div className="sidebar w-50 p-5 m-3 bg-[#F4F4F4] dark:bg-gray-800 rounded-lg ">
         <Sidebar setcat={setcat} />
     </div>
